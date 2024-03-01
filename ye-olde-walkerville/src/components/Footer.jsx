eee90d1ee450a2173f7d35c45b066e99f13bedaa
EyeAtthasatsri <133437896+EyeAtthasatsri@users.noreply.github.com>
2024-03-01 14:10:53 -0500

    update Footer&icons

--- ye-olde-walkerville/src/components/Footer.jsx
+++ ye-olde-walkerville/src/components/Footer.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
+import Logo_YeOlde from '../assests/img/Logo_YeOlde.png';
+import Footer_Location_icon from '../assests/img/Footer_Location_icon.png';
+import Footer_Phone_icon from '../assests/img/Footer_Phone_icon.png';
+import Footer_Email_icon from '../assests/img/Footer_Email_icon.png'
+
+
 
 const Footer = () => {
   return (
     <div className="footer my-2">
-        <div class="flex gap-4 justify-center items-center">
-  <div class="flex flex-col justify-center items-center bg-zinc-50 w-60"><img src="https://cdn5.vectorstock.com/i/1000x1000/33/44/location-icon-vector-24263344.jpg" class="ustify-center content-center"alt="" height="100px" width="100px"/>
+        <div class="flex gap-4 p-5 justify-center items-center">
+  <div class="flex flex-col justify-center items-center bg-transparent w-60"><img src={Footer_Location_icon} class="ustify-center content-center"alt="" height="100px" width="100px"/>
             <p class='text-center my-2'>1104 Monmouth Rd,
 Windsor, ON N8Y 3L8</p></div>
   <div class="w-60">
-    <img src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708783200&semt=ais" alt="" />
+    <img src={Logo_YeOlde} alt="" />
   </div>
   <div class="w-60 flex flex-col gap-6 justify-center">
     <div class="first-line flex gap-2">
-      <img src="https://png.pngtree.com/png-clipart/20191120/original/pngtree-phone-icon-png-image_5065646.jpg" alt="" height="30px" width="30px" />
+      <img src={Footer_Phone_icon} alt="" height="30px" width="30px" />
       <p>(519) 254-1507</p>
     </div>
     <div class="first-line flex gap-2">
-      <img src="https://i.etsystatic.com/26743355/r/il/69fb44/3074704892/il_570xN.3074704892_qh7k.jpg" alt="" height="30px" width="30px" />
+      <img src={Footer_Email_icon} alt="" height="30px" width="30px" />
       <p>yeolde@walkervillebb.com</p>
     </div>
   </div>
